c14603394ceee93802f62b69dfada54f63b7e17b
Jezhora <jezhora.photo@gmail.com>
2023-09-06 17:24:47 +0300

    feat: game done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,72 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.value = Math.round((this.min + this.max)/2)
+    return this.value;
+  }
 
-    guess() {
+  lower() {
+    return this.max = this.value; //if num > value
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return this.min = this.value; //if num < value
+  }
 }
+
+
+  // // 409
+  // game.setRange(0, 4048)
+  // let result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+
+  // // 782
+  // const game = new GuessingGame();
+  // game.setRange(0, 3198)
+  // let result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.greater();
+  // result = game.guess();
+  // game.lower();
+  // result = game.guess();
+  // console.log(game.guess())
 
 module.exports = GuessingGame;
